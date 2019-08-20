14625cb064bd7d91b49025352e1d0dbc93e414c2
Mert <mertturkmenoglu99@gmail.com>
2019-08-20 13:58:49 +0300

    Add basic variable example

---
+++ E003_Variables/main.clj
@@ -0,0 +1,13 @@
+(ns E003_Variables)
+
+;; Define integer, floating-number and string type variables
+(defn main []
+    (def int_var 5)
+    (def float_var 3.14)
+    (def str_var "Emily")
+
+    (println int_var)
+    (println float_var)
+    (println str_var))
+
+(main)
