6a8c61175719a3d9fce7bd3f0bfa230fee3fab2a
Mert <mertturkmenoglu99@gmail.com>
2019-08-22 17:33:24 +0300

    Add operators example

---
+++ E004_Operators/main.clj
@@ -0,0 +1,21 @@
+(ns E004_Variables)
+
+;; Operators example
+(defn main []
+    (println (+ 3 4))
+    (println (- 3 4))
+    (println (* 3 4))
+    (println (/ 8 2))
+    (println (inc 5))
+    (println (dec 5))
+    (println (max 1 2 3 4 5))
+    (println (min 1 2 3 4 5))
+    (println (rem 4 3))
+    (println (= 4 4))
+    (println (not= 3 4))
+    (println (and true false))
+    (println (or true false))
+    (println (not true)))
+
+
+(main)
